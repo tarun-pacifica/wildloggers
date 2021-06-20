d027f0be7f961bc35e1557025004b70710be04b4
Tarun Mookhey <tarun.mookhey@reinteractive.net>
2021-06-20 16:00:54 +0530

    map with lines and outbound

--- geocoder.rb
+++ geocoder.rb
@@ -5,5 +5,5 @@ location_list = ["Delhi, India", "Gorakhpur, India", "Siliguri, India", "Guwahat
 
 location_list.each_with_index do |location, index|
 	results = Geocoder.search location
-	puts "{text: 'Day #{index+1}: #{location}', lat: #{results.first.coordinates[0]}, long: #{results.first.coordinates[1]}}, " 
+	puts "{text: 'Day #{index+1}', lat: #{results.first.coordinates[0]}, long: #{results.first.coordinates[1]}}, " 
 end
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-    function createCirclePushpin(location, radius, fillColor, strokeColor, strokeWidth) {
+    function createCirclePushpin(text, location, radius, fillColor, strokeColor, strokeWidth) {
         strokeWidth = strokeWidth || 0;
 
         //Create an SVG string of a circle with the specified radius and color.
@@ -9,6 +9,7 @@
 
         //Create a pushpin from the SVG and anchor it to the center of the circle.
         return new Microsoft.Maps.Pushpin(location, {
+        	text : text,
             icon: svg.join(''),
             anchor: new Microsoft.Maps.Point(radius, radius)
         });
@@ -34,36 +35,39 @@
 
     function mapper() {
         let map = window.map = new Microsoft.Maps.Map('#map-grid', {
-            zoom: 5
+            zoom: 6
         });
 
         window.locationPins = []
 
         let locations = window.locations = [
-            { text: 'Day 1: Delhi, India', lat: 28.6517178, long: 77.2219388 },
-            { text: 'Day 2: Gorakhpur, India', lat: 26.67132865, long: 83.36458284327038 },
-            { text: 'Day 3: Siliguri, India', lat: 26.7164127, long: 88.4309916 },
-            { text: 'Day 4: Guwahati, India', lat: 26.1805978, long: 91.753943 },
-            { text: 'Day 5: Shillong, India', lat: 25.5760446, long: 91.8825282 },
-            { text: 'Day 6: Cherrapunji, India', lat: 25.2837969, long: 91.7193603 },
-            { text: 'Day 7: Tezpur, India', lat: 26.61695705, long: 92.76500717365028 },
-            { text: 'Day 8: Dirang, India', lat: 27.36008, long: 92.24121 },
-            { text: 'Day 9: Tawang, India', lat: 27.5879186, long: 91.863733 }
+            { text: '1', lat: 28.6517178, long: 77.2219388 },
+            { text: '2', lat: 26.67132865, long: 83.36458284327038 },
+            { text: '3', lat: 26.7164127, long: 88.4309916 },
+            { text: '4', lat: 26.1805978, long: 91.753943 },
+            { text: '5', lat: 25.5760446, long: 91.8825282 },
+            { text: '6', lat: 25.2837969, long: 91.7193603 },
+            { text: '7', lat: 26.61695705, long: 92.76500717365028 },
+            { text: '8', lat: 27.36008, long: 92.24121 },
+            { text: '9', lat: 27.5879186, long: 91.863733 }
         ]
 
         map.setView({
-            center: new Microsoft.Maps.Location(locations[0].lat, locations[0].long)
+            center: new Microsoft.Maps.Location(locations[1].lat, locations[1].long)
         })
 
         locations.forEach(function(item) {
             var location = new Microsoft.Maps.Location(item.lat, item.long);
             window.locationPins.push(location);
-            const pin = createCirclePushpin(location, 10, 'rgb(136, 220, 238)', 'black', 1)
-	        // var polyline = new Microsoft.Maps.Polyline(location: location);
+            const pin = createCirclePushpin(item.text, location, 12, 'rgb(136, 220, 238)', 'black', 1)
+            // var polyline = new Microsoft.Maps.Polyline(location: location);
             map.entities.push(pin);
             addHandler(pin);
         });
-        let polyline = new Microsoft.Maps.Polyline(window.locationPins);
+        let polyline = new Microsoft.Maps.Polyline(window.locationPins, {
+            strokeColor: 'rgb(136, 220, 238)',
+            strokeThickness: 3
+        });
         map.entities.push(polyline);
     };
 
