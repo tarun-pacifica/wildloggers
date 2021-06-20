2863b66c186b5264177abbee4725c3a6af180c35
Tarun Mookhey <tarun.mookhey@reinteractive.net>
2021-06-20 14:49:00 +0530

    map with dummy data for pins

---
+++ geocoder.rb
@@ -0,0 +1,9 @@
+require 'geocoder'
+require 'pry-byebug'
+
+location_list = ["Delhi, India", "Gorakhpur, India", "Siliguri, India", "Guwahati, India", "Shillong, India", "Cherrapunji, India", "Tezpur, India", "Dirang, India", "Tawang, India"]
+
+location_list.each_with_index do |location, index|
+	results = Geocoder.search location
+	puts "{text: 'Day #{index+1}: #{location}', lat: #{results.first.coordinates[0]}, long: #{results.first.coordinates[1]}}, " 
+end
--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 function mapper() {
-    const map = new Microsoft.Maps.Map('#map-grid');	
-}
+    let map = window.map = new Microsoft.Maps.Map('#map-grid', {
+        zoom: 10
+    });
 
-// document.addEventListener('DOMContentLoaded', (event) => {
-// 	mapper();
-// });
+    let locations = window.locations = [
+        { text: 'Day 1: Delhi, India', lat: 28.6517178, long: 77.2219388 },
+        { text: 'Day 2: Gorakhpur, India', lat: 26.67132865, long: 83.36458284327038 },
+        { text: 'Day 3: Siliguri, India', lat: 26.7164127, long: 88.4309916 },
+        { text: 'Day 4: Guwahati, India', lat: 26.1805978, long: 91.753943 },
+        { text: 'Day 5: Shillong, India', lat: 25.5760446, long: 91.8825282 },
+        { text: 'Day 6: Cherrapunji, India', lat: 25.2837969, long: 91.7193603 },
+        { text: 'Day 7: Tezpur, India', lat: 26.61695705, long: 92.76500717365028 },
+        { text: 'Day 8: Dirang, India', lat: 27.36008, long: 92.24121 },
+        { text: 'Day 9: Tawang, India', lat: 27.5879186, long: 91.863733 }
+    ]
+
+    map.setView({
+        center: new Microsoft.Maps.Location(locations[0].lat, locations[0].long)
+    })
+
+    locations.forEach(function(item) {
+        var location = new Microsoft.Maps.Location(item.lat, item.long);
+        const pin = new Microsoft.Maps.Pushpin(location, { text: item.text, color: 'aqua' });
+        map.entities.push(pin);
+    });
+};
+
+// colour of map / multiple markers / clickable / display info
